2f4c4b9d1daab74157021a6fc4763a353dda6a9d
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-10-19 22:20:26 -0400

    Added in Default UserRepository
    Just in case.

---
+++ KillerrinToolkit.Authentication/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using KillerrinToolkit.Authentication.Models;
+using KillerrinToolkit.EFCore.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KillerrinToolkit.Authentication.Repositories
+{
+    public class UserRepository<C> : RepositoryBase<User> where C : DbContext
+    {
+        public UserRepository(C context) : base(context)
+        {
+        }
+    }
+}
