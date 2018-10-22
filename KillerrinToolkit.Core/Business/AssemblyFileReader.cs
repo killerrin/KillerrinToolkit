73fe0dcdfe06cf2b7b2e646dfb83b5dda4bfffe5
Andrew Godfroy <killerrin@users.noreply.github.com>
2018-10-22 16:00:07 -0400

    Create AssemblyFileReader.cs

---
+++ KillerrinToolkit.Core/Business/AssemblyFileReader.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillerrinToolkit.Core.Business
+{
+    public static class AssemblyFileReader
+    {
+        public static List<string> ReadFile(this Assembly assembly, string resourceName)
+        {
+            List<string> lines = new List<string>();
+
+            // Get Resource Stream
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                // Open File
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    // Read until end of stream
+                    while (!reader.EndOfStream)
+                    {
+                        lines.Add(reader.ReadLine());
+                    }
+                }
+            }
+
+            return lines;
+        }
+    }
+}
