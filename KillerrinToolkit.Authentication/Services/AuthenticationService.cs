d4447bd15c40acbea39598f252b517b0b9b56644
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-10-21 20:31:55 -0400

    Split Validation into its own service

--- KillerrinToolkit.Authentication/Services/AuthenticationService.cs
+++ KillerrinToolkit.Authentication/Services/AuthenticationService.cs
@@ -24,6 +24,14 @@ namespace KillerrinToolkit.Authentication.Services
             _authTokenRepository = authTokenRepository;
         }
 
+        protected int WorkFactor { get; set; } = 12;
+        public virtual void FakeHash() { BCrypt.Net.BCrypt.HashPassword("", WorkFactor); }
+        public virtual bool IsExpired(DateTime? utcExpiry)
+        {
+            if (utcExpiry == null) return false;
+            return utcExpiry >= DateTime.UtcNow;
+        }
+
         #region AuthToken
         public virtual bool VerifyAuthToken(string authToken, out IAuthToken authenticationToken)
         {
@@ -93,82 +101,8 @@ namespace KillerrinToolkit.Authentication.Services
         #endregion
 
         #region Password
-        protected int WorkFactor { get; set; } = 12;
         public virtual string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password, WorkFactor + _random.Next(0, 3));
         public virtual bool VerifyPassword(string password, string hash) => BCrypt.Net.BCrypt.Verify(password, hash);
         #endregion
-
-        #region Validations
-        public string UsernameRegex { get; set; } = @"^[a-zA-Z0-9\s]";
-        public virtual bool ValidateUsername(string username)
-        {
-            if (!IntHelpers.IsBetween(username.Length, 1, 40))
-                return false;
-
-            if (!Regex.IsMatch(username, UsernameRegex))
-                return false;
-
-            return true;
-        }
-
-        public string EmailRegex { get; set; } = @"^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[!#$%&'*+/=?`{|}~^-]+)*@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$";
-        public virtual bool ValidateEmail(string email)
-        {
-            if (Regex.IsMatch(email, EmailRegex))
-                return false;
-            return true;
-        }
-
-        public PasswordScore MinimumPasswordStrength { get; set; } = PasswordScore.Medium;
-        public virtual bool ValidatePassword(string password)
-        {
-            PasswordScore passwordStrength = CheckPasswordStrength(password);
-            Debug.WriteLine($"{passwordStrength.ToString()}");
-            if ((int)passwordStrength >= (int)MinimumPasswordStrength)
-                return true;
-            return false;
-        }
-        #endregion
-
-        public virtual void FakeHash() { BCrypt.Net.BCrypt.HashPassword("", WorkFactor); }
-        public virtual PasswordScore CheckPasswordStrength(string password)
-        {
-            int score = 0;
-
-            if (string.IsNullOrWhiteSpace(password) || password.Length < 1)
-                return PasswordScore.Blank;
-
-            if (password.Length < 4)
-                return PasswordScore.VeryWeak;
-
-            if (password.Length >= 8)
-                score++;
-            if (password.Length >= 10)
-                score++;
-
-            if (Regex.Match(password, @"\d", RegexOptions.ECMAScript).Success)
-            {
-                score++;
-            }
-
-            if (Regex.Match(password, @"[a-z]", RegexOptions.ECMAScript).Success && Regex.Match(password, @"[A-Z]", RegexOptions.ECMAScript).Success)
-            {
-                score++;
-            }
-
-            if (Regex.Match(password, @"[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]", RegexOptions.ECMAScript).Success)
-            {
-                score++;
-            }
-
-            Debug.WriteLine($"Password: {password}, Length: {password.Length}, Score: {score}");
-            return (PasswordScore)score;
-        }
-        public virtual bool IsExpired(DateTime? utcExpiry)
-        {
-            if (utcExpiry == null) return false;
-            return utcExpiry >= DateTime.UtcNow;
-        }
-
     }
 }
---
+++ KillerrinToolkit.Authentication/Services/ValidationService.cs
@@ -0,0 +1,78 @@
+﻿using KillerrinToolkit.Authentication.Models.Enums;
+using KillerrinToolkit.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace KillerrinToolkit.Authentication.Services
+{
+    public class ValidationService
+    {
+        public string UsernameRegex { get; set; } = @"^[a-zA-Z0-9\s]";
+        public virtual bool ValidateUsername(string username)
+        {
+            if (!IntHelpers.IsBetween(username.Length, 1, 40))
+                return false;
+
+            if (!Regex.IsMatch(username, UsernameRegex))
+                return false;
+
+            return true;
+        }
+
+        public string EmailRegex { get; set; } = @"^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[!#$%&'*+/=?`{|}~^-]+)*@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$";
+        public virtual bool ValidateEmail(string email)
+        {
+            if (Regex.IsMatch(email, EmailRegex))
+                return false;
+            return true;
+        }
+
+        public PasswordScore MinimumPasswordStrength { get; set; } = PasswordScore.Medium;
+        public virtual bool ValidatePassword(string password)
+        {
+            PasswordScore passwordStrength = CheckPasswordStrength(password);
+            Debug.WriteLine($"{passwordStrength.ToString()}");
+            if ((int)passwordStrength >= (int)MinimumPasswordStrength)
+                return true;
+            return false;
+        }
+
+        public virtual PasswordScore CheckPasswordStrength(string password)
+        {
+            int score = 0;
+
+            if (string.IsNullOrWhiteSpace(password) || password.Length < 1)
+                return PasswordScore.Blank;
+
+            if (password.Length < 4)
+                return PasswordScore.VeryWeak;
+
+            if (password.Length >= 8)
+                score++;
+            if (password.Length >= 10)
+                score++;
+
+            if (Regex.Match(password, @"\d", RegexOptions.ECMAScript).Success)
+            {
+                score++;
+            }
+
+            if (Regex.Match(password, @"[a-z]", RegexOptions.ECMAScript).Success && Regex.Match(password, @"[A-Z]", RegexOptions.ECMAScript).Success)
+            {
+                score++;
+            }
+
+            if (Regex.Match(password, @"[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]", RegexOptions.ECMAScript).Success)
+            {
+                score++;
+            }
+
+            Debug.WriteLine($"Password: {password}, Length: {password.Length}, Score: {score}");
+            return (PasswordScore)score;
+        }
+
+    }
+}
