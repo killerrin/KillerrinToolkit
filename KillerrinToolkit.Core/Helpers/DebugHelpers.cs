c8917ba8803caa68cfff2f40216a5822b5ad0e59
Andrew Godfroy <killerrin@users.noreply.github.com>
2018-10-22 15:41:47 -0400

    Update DebugHelpers.cs

--- KillerrinToolkit.Core/Helpers/DebugHelpers.cs
+++ KillerrinToolkit.Core/Helpers/DebugHelpers.cs
@@ -8,6 +8,18 @@ namespace KillerrinToolkit.Core.Helpers
     public class DebugHelpers
     {
         public static bool DebugMode { get { return Debugger.IsAttached; } }
+        public static bool DebugConditional
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+        
         public static void PrintTotaloMemoryInUse()
         {
             Debug.WriteLine("GC: TOTAL MEMORY {0}", GC.GetTotalMemory(false));
