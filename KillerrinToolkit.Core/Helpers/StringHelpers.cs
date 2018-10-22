b533faf255274b56eb7038b30468c9917320693d
Andrew Godfroy <killerrin@users.noreply.github.com>
2018-10-22 15:45:17 -0400

    Update StringHelpers.cs

--- KillerrinToolkit.Core/Helpers/StringHelpers.cs
+++ KillerrinToolkit.Core/Helpers/StringHelpers.cs
@@ -65,5 +65,16 @@ namespace KillerrinToolkit.Core.Helpers
 
             return outputString;
         }
+        
+        public static string UrlCombine(string url1, string url2)
+        {
+            if (url1.Length == 0) { return url2; }
+            if (url2.Length == 0) { return url1; }
+
+            url1 = url1.TrimEnd('/', '\\');
+            url2 = url2.TrimStart('/', '\\');
+
+            return string.Format("{0}/{1}", url1, url2);
+        }
     }
 }
