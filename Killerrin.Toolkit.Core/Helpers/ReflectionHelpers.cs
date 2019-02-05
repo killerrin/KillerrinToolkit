b1d560caec52e51299653c3b83882084d4a737b7
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-02-05 12:11:29 -0500

    Added support for built in Version Number parsing

--- Killerrin.Toolkit.Core/Helpers/ReflectionHelpers.cs
+++ Killerrin.Toolkit.Core/Helpers/ReflectionHelpers.cs
@@ -7,11 +7,31 @@ using System.Threading.Tasks;
 
 namespace Killerrin.Toolkit.Core.Helpers
 {
-    /// <summary>
-    /// https://stackoverflow.com/questions/1196991/get-property-value-from-string-using-reflection-in-c-sharp
-    /// </summary>
     public static class ReflectionHelpers
     {
+        /// <summary>
+        /// Retrieves the Version Number from the specified assembly
+        /// </summary>
+        public static Version GetVersionNumber(Assembly assembly) {
+            if (assembly == null) assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            Version version = assembly.GetName().Version;
+            return version;
+        }
+        
+        /// <summary>
+        /// Retrieves the Version Number from the specified assembly using the default .NET Wildcard rules
+        /// </summary>
+        public static void GetVersionNumber(Assembly assembly, out Version version, out DateTime buildDate) {
+            
+            version = GetVersionNumber(assembly);
+            buildDate = new DateTime(2000, 1, 1)
+                .AddDays(version.Build)
+                .AddSeconds(version.Revision * 2);
+        }
+        
+        /// <summary>
+        /// https://stackoverflow.com/questions/1196991/get-property-value-from-string-using-reflection-in-c-sharp
+        /// </summary>
         public static object GetPropValue(this Object obj, String name)
         {
             foreach (String part in name.Split('.'))
@@ -27,6 +47,9 @@ namespace Killerrin.Toolkit.Core.Helpers
             return obj;
         }
 
+        /// <summary>
+        /// https://stackoverflow.com/questions/1196991/get-property-value-from-string-using-reflection-in-c-sharp
+        /// </summary>
         public static T GetPropValue<T>(this Object obj, String name)
         {
             Object retval = GetPropValue(obj, name);
