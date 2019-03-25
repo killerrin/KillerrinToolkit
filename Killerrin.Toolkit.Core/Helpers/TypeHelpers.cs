4b52ea196334906d27421049ae783495541acf9e
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-03-25 15:51:15 -0400

    Update TypeHelpers.cs

--- Killerrin.Toolkit.Core/Helpers/TypeHelpers.cs
+++ Killerrin.Toolkit.Core/Helpers/TypeHelpers.cs
@@ -10,13 +10,68 @@ namespace Killerrin.Toolkit.Core.Helpers
 {
     public static class TypeHelpers
     {
-        public static T TryParse<T>(string inValue)
+/// <summary>
+        /// Attempts to Parse the given input into the specified type 
+        /// </summary>
+        /// <typeparam name="T">The type you wish to convert your input into</typeparam>
+        /// <param name="input">The input you wish to convert into your Type</param>
+        /// <returns>The Parsed Object</returns>
+        /// <exception cref="NotSupportedException">NotSupportedException is thrown if the input can not be converted</exception>
+        public static T Parse<T>(string input)
         {
-            TypeConverter converter =
-                TypeDescriptor.GetConverter(typeof(T));
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            return (T)converter.ConvertFromString(null, CultureInfo.InvariantCulture, input);
+        }
 
-            return (T)converter.ConvertFromString(null,
-                CultureInfo.InvariantCulture, inValue);
+        /// <summary>
+        /// Attempts to Parse the given input into the specified type 
+        /// </summary>
+        /// <typeparam name="T">The type you wish to convert your input into</typeparam>
+        /// <param name="input">The input you wish to convert into your Type</param>
+        /// <returns>The Parsed Object</returns>
+        /// <exception cref="NotSupportedException">NotSupportedException is thrown if the input can not be converted</exception>
+        public static bool TryParse<T>(string input, out T output)
+        {
+            return Is<T>(input, out output);
+        }
+
+        /// <summary>
+        /// Determines if a given input can be converted into the given type
+        /// </summary>
+        /// <typeparam name="T">The type you wish to check if your input can convert into</typeparam>
+        /// <param name="input">The input you wish to check for converters</param>
+        /// <returns>Whether the item can be converted into the given type</returns>
+        public static bool Is<T>(this string input)
+        {
+            T converted = default(T);
+            return Is<T>(input, out converted);
+        }
+
+        /// <summary>
+        /// Determines if a given input can be converted into the given type
+        /// </summary>
+        /// <typeparam name="T">The type you wish to check if your input can convert into</typeparam>
+        /// <param name="input">The input you wish to check for converters</param>
+        /// <param name="converted">The converted object</param>
+        /// <returns>Whether the item can be converted into the given type</returns>
+        public static bool Is<T>(this string input, out T converted)
+        {
+            try
+            {
+                TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+                converted = (T)converter.ConvertFromString(null, CultureInfo.InvariantCulture, input);
+                return true;
+            }
+            catch (NotSupportedException)
+            {
+                converted = default(T);
+                return false;
+            }
+            catch (Exception)
+            {
+                converted = default(T);
+                return false;
+            }
         }
     }
 }
