e6b297c90d8540e0e1c79ae0b596efa9b3c6bae9
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-05-27 14:52:10 -0400

    Update DataBind.cs

--- Killerrin.Toolkit.Core/Models/DataBind.cs
+++ Killerrin.Toolkit.Core/Models/DataBind.cs
@@ -16,10 +16,32 @@ namespace Killerrin.Toolkit.Core.Models
             }
         }
 
+        public bool HasValue
+        {
+            get { return m_value != null; }
+        }
+
+        /// <summary>
+        /// Checks if the Value is null, and if the value is default(T)
+        /// </summary>
+        public bool IsDefault
+        {
+            get
+            {
+                if (!HasValue) return true;
+                return m_value.Equals(default(T));
+            }
+        }
+
         public DataBind() { }
         public DataBind(T value) { Value = value; }
-        public DataBind(T value, PropertyChangedEventHandler handler) { Value = value;  PropertyChanged = handler; }
-        public DataBind(DataBind<T> data) { Value = data.Value;  PropertyChanged = data.PropertyChanged; }
+        public DataBind(T value, PropertyChangedEventHandler handler) { Value = value; PropertyChanged = handler; }
+        public DataBind(DataBind<T> data)
+        {
+            Value = data.Value;
+            PropertyChanging = data.PropertyChanging;
+            PropertyChanged = data.PropertyChanged;
+        }
 
         public static implicit operator T(DataBind<T> data) { return data.Value; }
         public static implicit operator DataBind<T>(T data) { return new DataBind<T>(data); }
@@ -28,16 +50,41 @@ namespace Killerrin.Toolkit.Core.Models
         public override bool Equals(object obj) { return Value.Equals(obj); }
         public override int GetHashCode() { return Value.GetHashCode(); }
 
+        /// <summary>
+        /// Event Handler for Modelbinding on Property Changing. 
+        /// </summary>
+        /// <remarks>
+        /// This event handler fires multiple times in a row for all <see cref="Value"/>, <see cref="HasValue" and <see cref="NotDefault"/>.
+        /// </remarks>
         public event PropertyChangingEventHandler PropertyChanging;
+
+        /// <summary>
+        /// Event Handler for Modelbinding on Property Changed. 
+        /// </summary>
+        /// <remarks>
+        /// This event handler fires multiple times in a row for all <see cref="Value"/>, <see cref="HasValue" and <see cref="NotDefault"/>.
+        /// </remarks>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Special Event Handler that only triggers on <see cref="Value"/> Change.
+        /// </summary>
+        /// <remarks>
+        /// This event is only fired once.
+        /// </remarks>
+        public event PropertyChangedEventHandler ValueChanged;
 
         public void RaisePropertyChanging()
         {
             PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(Value)));
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(HasValue)));
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(IsDefault)));
         }
         public void RaisePropertyChanged()
         {
+            ValueChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HasValue)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsDefault)));
         }
-    }
 }
